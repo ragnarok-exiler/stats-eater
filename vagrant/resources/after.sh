4f1fb0a0e479c3dcd570d8a56809d528a59ccb98
Juan Salgado Vaquer <ragnarok-exiler@hotmail.com>
2018-06-03 01:51:09 +0200

    Add: Prepare laravel project to run after vagrant provision includes:     - Execution composer install     - Create .env  based on .env.example     - Generate key with artisan

--- vagrant/resources/after.sh
+++ vagrant/resources/after.sh
@@ -3,3 +3,10 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+cd ~/code/api
+composer install
+cp .env.example .env
+php artisan key:generate
+
+# cd ~/code/front
