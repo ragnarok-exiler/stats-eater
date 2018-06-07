7ebdb52dbd8525617de40e361ac20b2c81b417c4
Juan Salgado Vaquer <jsalgado@factorenergia.com>
2018-06-07 12:06:28 +0200

    Add: Initialize react project. Add: Autoinstall vagrant-hostmanager

--- vagrant/resources/after.sh
+++ vagrant/resources/after.sh
@@ -4,9 +4,17 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 
+# Initialize laravel project in  ~/code/api
 cd ~/code/api
 composer install
-cp .env.example .env
+rm .env
+sudo cp .env.example .env
 php artisan key:generate
 
-# cd ~/code/front
+# Initialize react project in ~/code/front
+sudo npm install -g create-react-app
+create-react-app /tmp/react_vagrant
+cd ~/code/front
+sudo rm -rf node_modules/
+cp -R /tmp/react_vagrant/node_modules/ node_modules/
+sudo rm -rf /tmp/react_vagrant
