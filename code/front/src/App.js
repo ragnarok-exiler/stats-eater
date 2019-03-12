bd10ae31aded670622a741d8e1f3e57be279e2de
Juan Salgado Vaquer <jsalgado@factorenergia.com>
2019-03-12 16:34:50 +0100

    React actualizado

--- code/front/src/App.js
+++ code/front/src/App.js
@@ -1,39 +1,23 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-
+import {Alert} from 'reactstrap';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            test: {},
-        };
-        this.getTokenUrl();
-
-    }
-
-    getTokenUrl() {
-        const url = 'http://stats-api/api/paladins/Especial';
-        axios.get(url)
-            .then(
-                res => {
-                    this.setState({test: res.data});
-                }
-            );
-    }
-
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to Reacting</h1>
+                    <p> Edit <code>src/App.js</code> and save to reload.</p>
+
                 </header>
-                <p className="App-intro"> To get started, edit <code>src/App.js</code> and save to reload.</p>
-                <p>The CreateSession Url is: {this.state.test.token}</p>
-                <button content="asdasdsa">kl</button>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Bienvenidos a casa
+                </a>
+                <div>
+                    <Alert color="primary">Adding new features</Alert>
+                </div>
             </div>
         );
     }
