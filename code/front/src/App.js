510cd43e850acfe9572a14277acadab7f6e09566
Juan Salgado Vaquer <jsalgado@factorenergia.com>
2019-03-12 18:28:07 +0100

    BS4 y reactstrap

--- code/front/src/App.js
+++ code/front/src/App.js
@@ -1,23 +1,26 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Alert} from 'reactstrap';
+import {Alert, Container, Row, Col} from 'reactstrap';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p> Edit <code>src/App.js</code> and save to reload.</p>
-
-                </header>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Bienvenidos a casa
-                </a>
-                <div>
-                    <Alert color="primary">Adding new features</Alert>
-                </div>
+                {/*<header className="App-header">*/}
+                {/*    <img src={logo} className="App-logo" alt="logo"/>*/}
+                {/*    <p> Edit <code>src/App.js</code> and save to reload. </p>*/}
+                {/*    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">*/}
+                {/*        Learn React*/}
+                {/*    </a>*/}
+                {/*</header>*/}
+                <Container fluid={true}>
+                    <Row>
+                        <Col>
+                            <Alert color="primary">asdsad</Alert>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
